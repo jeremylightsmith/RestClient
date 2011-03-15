e06abc34b468fb3cabb8823c8f33c4b3a1af284b
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2011-03-15 10:51:45 -0700

    Initial Commit

---
+++ RestClientTests/RestClientTests.h
@@ -0,0 +1,17 @@
+//
+//  RestClientTests.h
+//  RestClientTests
+//
+//  Created by Jeremy Lightsmith on 3/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface RestClientTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ RestClientTests/RestClientTests.m
@@ -0,0 +1,33 @@
+//
+//  RestClientTests.m
+//  RestClientTests
+//
+//  Created by Jeremy Lightsmith on 3/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RestClientTests.h"
+
+
+@implementation RestClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RestClientTests");
+}
+
+@end
